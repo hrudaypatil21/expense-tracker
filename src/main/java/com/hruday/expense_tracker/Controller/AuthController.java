d0acb427d92b6491c767e6092d4ba9b519dd898d
hrudaypatil21 <hrudaypatil21@gmail.com>
2025-05-27 18:02:10 +0530

    auth cont

--- src/main/java/com/hruday/expense_tracker/Controller/AuthController.java
+++ src/main/java/com/hruday/expense_tracker/Controller/AuthController.java
@@ -2,6 +2,8 @@ package com.hruday.expense_tracker.Controller;
 
 import com.hruday.expense_tracker.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +13,12 @@ public class AuthController {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
 
 }
