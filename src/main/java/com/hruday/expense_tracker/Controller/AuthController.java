768d5efe37d67029beba27f26e93115b7b365df8
hrudaypatil21 <hrudaypatil21@gmail.com>
2025-05-27 13:25:54 +0530

    auth user mod

---
+++ src/main/java/com/hruday/expense_tracker/Controller/AuthController.java
@@ -0,0 +1,16 @@
+package com.hruday.expense_tracker.Controller;
+
+import com.hruday.expense_tracker.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+}
--- src/main/java/com/hruday/expense_tracker/Model/User.java
+++ src/main/java/com/hruday/expense_tracker/Model/User.java
@@ -5,6 +5,14 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Entity
 @Getter
@@ -25,4 +33,44 @@ public class User {
 
     @Column(nullable = false)
     Long balance;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
+    @Column(name = "role")
+    private Set<String> roles = new HashSet<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toSet());
+    }
+
+    @Override
+    public String getUsername() {
+        return email.replaceAll("@.*", "");
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
+
+//    @Column(nullable = false, unique = true)
+//    String username = email.replaceAll("@.*", "");
