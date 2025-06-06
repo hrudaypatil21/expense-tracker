bdbf89ddbf99797b3d2ae3f8014692f1b18589c8
hrudaypatil21 <hrudaypatil21@gmail.com>
2025-06-06 16:33:48 +0530

    auth service

--- src/main/java/com/hruday/expense_tracker/Controller/AuthController.java
+++ src/main/java/com/hruday/expense_tracker/Controller/AuthController.java
@@ -1,11 +1,12 @@
 package com.hruday.expense_tracker.Controller;
 
+import com.hruday.expense_tracker.Model.User;
 import com.hruday.expense_tracker.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -20,5 +21,8 @@ public class AuthController {
     @Autowired
     private AuthenticationManager authenticationManager;
 
-
+    @PostMapping("/register")
+    private ResponseEntity<?> registerUser(@RequestBody User user) {
+        
+    }
 }
